2a35ea0dc0a15a0732fbc9c1a8b60fdf19178bb8
Eric Mayfield <eric@visiblycreative.com>
2019-01-22 14:01:37 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,14 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    student = set_student
+    student.active = !set_student.active
+    student.save
+    redirect_to action: "show", id: params[:id]
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create!(first_name: "Eric", last_name: "Mayfield")
+Student.create!(first_name: "Kiki", last_name: "Mayfield")
+Student.create!(first_name: "Daviel", last_name: "Banister")
